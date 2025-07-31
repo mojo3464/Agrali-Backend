2ab111af9cc45792224011b49c800732124d7c50
mojo3464 <138800746+mojo3464@users.noreply.github.com>
2025-07-31 15:03:51 +0300

    Update validatorMiddleware.js

--- middlewares/validatorMiddleware.js
+++ middlewares/validatorMiddleware.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 const validatrorMiddleware = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({status: 'error", errors: errors.array()[0]});
+        return res.status(400).json({status: 'error', errors: errors.array()[0]});
     }
     next();
 }
